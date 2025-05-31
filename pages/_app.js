812d585823b26fd3c280a1b74073210cdb36485b
Momex94 <momcilo.panic26@gmail.com>
2025-05-31 20:15:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,3 @@
-import { useEffect } from "react";
-import '../styles/globals.css';
-
-
-function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window !== "undefined" && window.Pi?.init) {
-      window.Pi.init({
-        version: "2.0",
-      });
-    }
-  }, []);
-
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
